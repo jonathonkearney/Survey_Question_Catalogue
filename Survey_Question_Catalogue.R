e3819a53a17fbea93326ba875e0553050dfb7c3e
jonathonkearney <Jonathon.kearney@gmail.com>
2022-11-02 00:33:44 +1300

    Add files via upload

--- Survey_Question_Catalogue.R
+++ Survey_Question_Catalogue.R
@@ -17,38 +17,38 @@ questionData <- read.csv("Question_Data.csv", header = TRUE, check.names=FALSE)
 questions <- questions %>% mutate_all(na_if,"")
 questionData <- questionData %>% mutate_all(na_if,"")
 
-tags <- list(questions$Tag1, questions$Tag2, questions$Tag3, questions$Tag4)
-tags <- Reduce(c,tags)
-tags <- unique(tags)
-tags <- tags[!is.na(tags)]
+uniqueTags <- list(questions$Tag1, questions$Tag2, questions$Tag3, questions$Tag4)
+uniqueTags <- Reduce(c,uniqueTags)
+uniqueTags <- unique(uniqueTags)
+uniqueTags <- uniqueTags[!is.na(uniqueTags)]
 
 ui <- fluidPage(
-  
   theme = shinytheme("flatly"),
-  
   navbarPage("Survey Questions Catalogue",
       tabPanel("Survey Questions",
           sidebarLayout(
               sidebarPanel(
                   width = "3",
-                      pickerInput("tags", "Select one or more tags", sort(tags), options = list(`actions-box` = TRUE), selected= tags, multiple=TRUE),
+                      pickerInput("uniqueTags", "Select one or more uniqueTags", sort(uniqueTags), options = list(`actions-box` = TRUE), selected= uniqueTags, multiple=TRUE),
                       selectInput("date","Select a date", choices = c("All", unique(sort(questions$Date))), selected = "All"),
                       selectInput("location", "Select a location", choices = unique(sort(questions$Location)), selected = "All"),
                       selectInput("quest","Select a question", choices = c(unique(questions$Question)), selected = "After how many years do you replace your phone?")
               ),
-                        
+
                       mainPanel(
                         width = "6",
-                          h4(em(textOutput(outputId = "surveyNameText"))),
-                          h4(em(textOutput(outputId = "locationText"))),
-                          h4(em(textOutput(outputId = "sampleSizeText"))),
-                          h4(em(textOutput(outputId = "dateText"))),
-                          h4(em(textOutput(outputId = "dataTypeText"))),
-                          h5(a(textOutput(outputId = "linkText"))),
-                          headerPanel(""),
-                          DT::dataTableOutput("table"),
-                          headerPanel(""),
-                          plotOutput(outputId = "plot",width = "1000px", height = "500px")
+                          h5(em(textOutput(outputId = "surveyNameText"))),
+                          h5(em(textOutput(outputId = "locationText"))),
+                          h5(em(textOutput(outputId = "sampleSizeText"))),
+                          h5(em(textOutput(outputId = "dateText"))),
+                          h5(em(textOutput(outputId = "dataTypeText"))),
+                          h6(a(textOutput(outputId = "linkText"))),
+                          HTML("<hr>"),
+                          # headerPanel(""),
+                          DT::dataTableOutput("table", width = "1000px"),
+                          HTML("<hr>"),
+                          # headerPanel(""),
+                          plotOutput(outputId = "plot", width = "1000px", height = "500px")
                       )
               )
       )
@@ -61,7 +61,7 @@ server <- function(input, output, session) {
     
     filteredSelections <- questions
     
-    filteredSelections <- filter(filteredSelections, questions$Tag1 %in% input$tags)
+    filteredSelections <- filter(filteredSelections, questions$Tag1 %in% input$uniqueTags)
     
     if(input$date != "All"){
       filteredSelections <- filter(filteredSelections, Date == input$date)
