6c9039df80d69cd3039df619e24d41cc588f8253
jonathonkearney <Jonathon.kearney@gmail.com>
2022-11-02 10:04:52 +1300

    Add files via upload

--- Survey_Question_Catalogue.R
+++ Survey_Question_Catalogue.R
@@ -12,10 +12,11 @@ rm(list = ls())
 setwd("C:/Users/OEM/OneDrive/Documents/R/Survey_Question_Catalogue")
 
 questions <- read.csv("Questions.csv", header = TRUE)
-questionData <- read.csv("Question_Data.csv", header = TRUE, check.names=FALSE)
+
+surveys <- list.files(path = "C:/Users/OEM/OneDrive/Documents/R/Survey_Question_Catalogue/Survey_Data", pattern = '.csv$', full.names = T) %>%
+  map(read_csv)
 
 questions <- questions %>% mutate_all(na_if,"")
-questionData <- questionData %>% mutate_all(na_if,"")
 
 uniqueTags <- list(questions$Tag1, questions$Tag2, questions$Tag3, questions$Tag4)
 uniqueTags <- Reduce(c,uniqueTags)
@@ -44,17 +45,8 @@ ui <- fluidPage(
 
                       mainPanel(
                         width = "6",
-                          # h5(em(textOutput(outputId = "surveyNameText"))),
-                          # h5(em(textOutput(outputId = "locationText"))),
-                          # h5(em(textOutput(outputId = "sampleSizeText"))),
-                          # h5(em(textOutput(outputId = "dateText"))),
-                          # h5(em(textOutput(outputId = "dataTypeText"))),
-                          # h6(a(textOutput(outputId = "linkText"))),
-                          # HTML("<hr>"),
-                          # headerPanel(""),
                           DT::dataTableOutput("table", width = "1000px"),
                           HTML("<hr>"),
-                          # headerPanel(""),
                           plotOutput(outputId = "plot", width = "1000px", height = "500px")
                       )
               )
@@ -89,44 +81,52 @@ server <- function(input, output, session) {
   output$surveyNameText <- renderText({ paste0("Survey Name: ", questions$Survey_Name[questions$Question == input$quest]) })
   output$locationText <- renderText({ paste0("Location: ", questions$Location[questions$Question == input$quest]) })
   output$dateText <- renderText({ paste0("Date: ", questions$Date[questions$Question == input$quest]) })
-  output$sampleSizeText <- renderText({ paste0("Sample Size: ", length(questionData[, input$quest][!is.na(questionData[, input$quest])])) })
+  output$sampleSizeText <- renderText({
+    DFLocation <- which(sapply(surveys, function(x) any(names(x) == input$quest)))
+    survey <- as.data.frame(surveys[DFLocation], check.names=FALSE)
+    paste0("Sample Size: ", length(survey[, input$quest][!is.na(survey[, input$quest])])) 
+    
+  })
   output$dataTypeText <- renderText({ paste0("Data Type: ", questions$Data_Type[questions$Question == input$quest]) })
   output$linkText <- renderText({ paste0("Survey Link: ", questions$Link[questions$Question == input$quest]) })
   
   output$table = DT::renderDataTable({
     
     dataType <- questions$Data_Type[questions$Question == input$quest]
+    DFLocation <- which(sapply(surveys, function(x) any(names(x) == input$quest)))
+    survey <- as.data.frame(surveys[DFLocation], check.names=FALSE)
     
     if(dataType == "Nominal"){
-      tbl <- as.data.frame(table(questionData[, input$quest]))
+      tbl <- as.data.frame(table(survey[, input$quest]))
       colnames(tbl) <- c(input$quest , "Count")
     }
     
     if(dataType == "Ratio"){
-      tbl <- as.data.frame(describe(questionData[, input$quest]))
+      tbl <- as.data.frame(describe(survey[, input$quest]))
       tbl = subset(tbl, select = -c(skew, kurtosis, se, trimmed))
     }
     
-    DT::datatable(tbl, rownames = FALSE, options = list(searching = FALSE, dom = 'f', pageLength = 25))
+    DT::datatable(tbl, rownames = FALSE, options = list(searching = FALSE, dom = 'p', pageLength = 10))
   })
 
   output$plot <- renderPlot({
     
     dataType <- questions$Data_Type[questions$Question == input$quest]
+    DFLocation <- which(sapply(surveys, function(x) any(names(x) == input$quest)))
+    survey <- as.data.frame(surveys[DFLocation], check.names=FALSE)
     
     if(dataType == "Nominal"){
-      tbl <- as.data.frame(table(questionData[, input$quest]))
+      tbl <- as.data.frame(table(survey[, input$quest]))
       p <- ggplot(tbl, aes(Var1, Freq, fill = Var1)) +
         geom_bar(stat='identity') + labs(x = input$quest)
     }
     
     if(dataType == "Ratio"){
-      p <- ggplot(questionData, aes(x = questionData[,input$quest])) +
+      p <- ggplot(survey, aes(x = survey[,input$quest])) +
         geom_histogram(binwidth=1, colour="black", fill="cadetblue2") + labs(x = input$quest)
     }
     
     p
-    
   })
 }
 
