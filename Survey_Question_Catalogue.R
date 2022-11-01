22b068d8670c1f95d16a58405295a2c03d908d0e
jonathonkearney <Jonathon.kearney@gmail.com>
2022-11-02 11:39:48 +1300

    Add files via upload

--- Survey_Question_Catalogue.R
+++ Survey_Question_Catalogue.R
@@ -3,7 +3,7 @@ library(shiny)
 library(shinythemes)
 library(DT)
 library(shinyWidgets)
-require(psych)  
+require(psych)
 
 
 # *********************** LOAD & CLEAN DATA ***************************
@@ -24,13 +24,21 @@ uniqueTags <- unique(uniqueTags)
 uniqueTags <- uniqueTags[!is.na(uniqueTags)]
 
 ui <- fluidPage(
+  tags$head(
+    # Note the wrapping of the string in HTML()
+    tags$style(HTML("
+      .table, hr {
+        margin-left: 24px;
+      }
+      "))
+  ),
   theme = shinytheme("flatly"),
   navbarPage("Survey Questions Catalogue",
-      tabPanel("Survey Questions",
+      tabPanel("Question Viewer",
           sidebarLayout(
               sidebarPanel(
                   width = "3",
-                      pickerInput("uniqueTags", "Select one or more Tags", sort(uniqueTags), options = list(`actions-box` = TRUE), multiple=TRUE),
+                      pickerInput("uniqueTags", "Select one or more Tags", sort(uniqueTags), options = list(`actions-box` = TRUE), selected = uniqueTags, multiple=TRUE),
                       selectInput("date","Select a date", choices = c("All", unique(sort(questions$Date))), selected = "All"),
                       selectInput("location", "Select a location", choices = unique(sort(questions$Location)), selected = "All"),
                       selectInput("quest","Select a question", choices = c(unique(questions$Question)), selected = "After how many years do you replace your phone?"),
@@ -45,7 +53,10 @@ ui <- fluidPage(
 
                       mainPanel(
                         width = "6",
-                          DT::dataTableOutput("table", width = "1000px"),
+                          h2(textOutput(outputId = "questionNameText")),
+                          HTML("<hr>"),
+                          # DT::dataTableOutput("table", width = "1000px"),
+                          tableOutput("table"),
                           HTML("<hr>"),
                           plotOutput(outputId = "plot", width = "1000px", height = "500px")
                       )
@@ -55,6 +66,14 @@ ui <- fluidPage(
 )
 
 server <- function(input, output, session) {
+  
+  dataType <- "Unknown"
+  
+  observe({
+    dataType <- questions$Data_Type[questions$Question == input$quest]
+  })
+  
+  # *********************** DYNAMIC INPUTS ***************************
   
   observe({
     
@@ -78,6 +97,8 @@ server <- function(input, output, session) {
     )
   })
   
+  # *********************** SURVEY INFORMATION ***************************
+  
   output$surveyNameText <- renderText({ paste0("Survey Name: ", questions$Survey_Name[questions$Question == input$quest]) })
   output$locationText <- renderText({ paste0("Location: ", questions$Location[questions$Question == input$quest]) })
   output$dateText <- renderText({ paste0("Date: ", questions$Date[questions$Question == input$quest]) })
@@ -90,8 +111,11 @@ server <- function(input, output, session) {
   output$dataTypeText <- renderText({ paste0("Data Type: ", questions$Data_Type[questions$Question == input$quest]) })
   output$linkText <- renderText({ paste0("Survey Link: ", questions$Link[questions$Question == input$quest]) })
   
-  output$table = DT::renderDataTable({
-    
+  output$questionNameText <- renderText({ questions$Question[questions$Question == input$quest] })
+
+  # *********************** DATA TYPES ***************************
+  
+  observe({
     dataType <- questions$Data_Type[questions$Question == input$quest]
     DFLocation <- which(sapply(surveys, function(x) any(names(x) == input$quest)))
     survey <- as.data.frame(surveys[DFLocation], check.names=FALSE)
@@ -99,34 +123,43 @@ server <- function(input, output, session) {
     if(dataType == "Nominal"){
       tbl <- as.data.frame(table(survey[, input$quest]))
       colnames(tbl) <- c(input$quest , "Count")
+      
+      
+      output$table <- renderTable({
+        tbl
+      })
+      
+      
+      output$plot <- renderPlot({
+        ggplot(tbl, aes(tbl[, input$quest], Count , fill = tbl[, input$quest])) +
+          geom_bar(stat='identity') + labs(x = input$quest, fill = input$quest)
+      })
+    }
+    
+    if(dataType == "Ordinal"){
+      
+    }
+    
+    if(dataType == "Interval"){
+      
     }
     
     if(dataType == "Ratio"){
+      
+      
       tbl <- as.data.frame(describe(survey[, input$quest]))
-      tbl = subset(tbl, select = -c(skew, kurtosis, se, trimmed))
+      tbl = subset(tbl, select = -c(vars, skew, kurtosis, se, trimmed))
+      tbl <- gather(tbl, Statistic, Value, n:range)
+      
+      output$table <- renderTable({
+        tbl
+      })
+      
+      output$plot <- renderPlot({
+        ggplot(survey, aes(x = survey[,input$quest])) +
+          geom_histogram(binwidth=1, colour="black", fill="cadetblue2") + labs(x = input$quest)
+      })
     }
-    
-    DT::datatable(tbl, rownames = FALSE, options = list(searching = FALSE, dom = 'p', pageLength = 10))
-  })
-
-  output$plot <- renderPlot({
-    
-    dataType <- questions$Data_Type[questions$Question == input$quest]
-    DFLocation <- which(sapply(surveys, function(x) any(names(x) == input$quest)))
-    survey <- as.data.frame(surveys[DFLocation], check.names=FALSE)
-    
-    if(dataType == "Nominal"){
-      tbl <- as.data.frame(table(survey[, input$quest]))
-      p <- ggplot(tbl, aes(Var1, Freq, fill = Var1)) +
-        geom_bar(stat='identity') + labs(x = input$quest)
-    }
-    
-    if(dataType == "Ratio"){
-      p <- ggplot(survey, aes(x = survey[,input$quest])) +
-        geom_histogram(binwidth=1, colour="black", fill="cadetblue2") + labs(x = input$quest)
-    }
-    
-    p
   })
 }
 
